c5dfaa343b6d796ae0080ca2b213ac1c8ed735f2
Gerald Marechal <gerald1973@gmail.com>
2021-04-04 19:36:06 +0200

    ok

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # bit_to_text
-Program to represent in text format som series of bits.
+
+## Description
+
+Use this program to represent some characters set from the memory of an old computer (ex. Commodore 64).  
+
+### example
+
+```shell
+./bittext ../Downloads/characters.901225-01.bin
+
+0c80 03200 255 ff ********
+0c81 03201 255 ff ********
+0c82 03202 131 83 *.....**
+0c83 03203 153 99 *..**..*
+0c84 03204 153 99 *..**..*
+0c85 03205 131 83 *.....**
+0c86 03206 159 9f *..*****
+0c87 03207 159 9f *..*****
+
+0c88 03208 255 ff ********
+0c89 03209 255 ff ********
+0c8a 03210 193 c1 **.....*
+0c8b 03211 153 99 *..**..*
+0c8c 03212 153 99 *..**..*
+0c8d 03213 193 c1 **.....*
+0c8e 03214 249 f9 *****..*
+0c8f 03215 249 f9 *****..*
+
+```
+
---
+++ bittext.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdint.h>
+
+char tmp_binary[9];
+int counter_line = 0;
+
+/**
+ * @brief Convert a byte to a binary string.
+ * 
+ * @param bytes 
+ * @param result 
+ */
+void write_binary(uint8_t byte, char *result)
+{
+    uint8_t tmp = byte;
+    for (int i = 0; i < 8; i++)
+    {
+        if (tmp % 2 == 0)
+        {
+            result[7 - i] = '.';
+        }
+        else
+        {
+            result[7 - i] = '*';
+        }
+        tmp = tmp >> 1;
+    }
+}
+
+void print_buffer(uint8_t *buffer, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%04x %05d %03d %02x ", counter_line, counter_line, buffer[i], buffer[i]);
+        write_binary(buffer[i], tmp_binary);
+        printf("%s\n", tmp_binary);
+        counter_line++;
+        if (counter_line % 8 == 0)
+        {
+            printf("\n");
+        }
+    }
+}
+
+void read_bin_file(char *fileName)
+{
+    static const int BUFFER_SIZE = 8;
+    int read_bytes = 0;
+    int i;
+    FILE *file;
+    uint8_t buffer[BUFFER_SIZE];
+
+    file = fopen(fileName, "rb");
+    if (file)
+    {
+        while ((read_bytes = fread(buffer, 1, BUFFER_SIZE, file)) == BUFFER_SIZE)
+        {
+            print_buffer(buffer, read_bytes);
+        };
+        if (read_bin_file > 0)
+        {
+            print_buffer(buffer, read_bytes);
+        }
+    }
+    fclose(file);
+}
+
+int main(int argc, char *argv[])
+{
+    int result = 0;
+    if (argc != 2)
+    {
+        printf("Wrong number of arguments.\n");
+        printf("example:\n");
+        printf("bittext my_file.bin\n");
+        result = 1;
+    }
+    else
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            tmp_binary[i] = '0';
+        }
+        tmp_binary[8] = 0;
+        printf("File to parse: %s\n", argv[1]);
+        read_bin_file(argv[1]);
+    }
+    return result;
+}
